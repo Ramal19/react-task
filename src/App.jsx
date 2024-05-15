6b07e50587f344cde2ff89ca65d8dedc3fb22f92
Ramal19 <ld4xz51vd@code.edu.az>
2024-05-15 08:23:17 +0400

    taskkkk

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <>
+      <header>
+        <button>Why Accupay</button>
+        <div className="head">
+          <h4>A platform for your global team</h4>
+          <p>Your dependable guide to achieving freedom from manual HR work and building that perfect workplace you have always aspired to build. Your dependable guide to achieving freedom.</p>
+        </div>
+        <div className="container">
+          <div>
+            <img src="https://softivuspro.com/accupay/main/assets/images/whyAccoupayCard_1.png"/>
+            <h4>Hire or relocate team member with in house visa support</h4>
+          </div>
+          <div>
+            <img src="https://softivuspro.com/accupay/main/assets/images/whyAccoupayCard_2.png"/>
+            <h4>Create complaint contracts with a single click</h4>
+          </div>
+          <div>
+            <img src="https://softivuspro.com/accupay/main/assets/images/whyAccoupayCard_3.png"/>
+            <h4>Send equipment worldwide, without the hassle</h4>
+          </div>
+          <div>
+            <img src="https://softivuspro.com/accupay/main/assets/images/whyAccoupayCard_3.png"/>
+            <h4>Send equipment worldwide, without the hassle</h4>
+          </div>
+          <div>
+            <img src="https://softivuspro.com/accupay/main/assets/images/whyAccoupayCard_3.png"/>
+            <h4>Send equipment worldwide, without the hassle</h4>
+          </div>
+          <div>
+            <img src="https://softivuspro.com/accupay/main/assets/images/whyAccoupayCard_3.png"/>
+            <h4>Send equipment worldwide, without the hassle</h4>
+          </div>
+        </div>
+      </header>
+
+      <section>
+        <div className="headO">
+          <a href="">Experience AccuPay</a>
+          <h4>We’ve got everything you need?</h4>
+          <p>We save you from all that boring paperwork! From global hiring, to instant payments, and taxes, we’ve got your back.</p>
+          <button>Contact Us</button>
+        </div>
+        <img src="https://softivuspro.com/accupay/main/assets/images/contact_illus.png" alt="" />
+      </section>
+    </>
+  )
+}
